470be0c923955f7590c926b0860faa1a6f13ebfe
Ray Donnelly <mingw.android@gmail.com>
2018-11-17 00:31:55 +0000

    Windows fixes from whoever worked on this last

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,4 @@ export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
 
-if [[ `uname` == 'Darwin' ]]; then
-    export PATH="$SRC_DIR:$PATH"
-    cp $RECIPE_DIR/clang_wrapper.sh $SRC_DIR/clang
-    chmod +x $SRC_DIR/clang
-fi
-
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
