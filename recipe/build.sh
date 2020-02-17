a9b9d7bd31313d6fbdab5307e1105ea739dd87cb
Jonathan Helmus <jjhelmus@gmail.com>
2020-02-17 16:03:30 -0600

    update to 1.27.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,4 +7,7 @@ export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
 
+# setup.py uses cc rather than $CC in some cases
+ln -s ${CC} ${BUILD_PREFIX}/bin/cc
+
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
