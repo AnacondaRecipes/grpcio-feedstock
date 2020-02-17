3430b24415417cab3adfc5b0c5d64916d47d438a
Jonathan Helmus <jjhelmus@gmail.com>
2020-02-17 16:21:13 -0600

    use cython during build

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ export GRPC_PYTHON_LDFLAGS=""
 export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
+export GRPC_PYTHON_BUILD_WITH_CYTHON="True"
 
 # setup.py uses cc rather than $CC in some cases
 ln -s ${CC} ${BUILD_PREFIX}/bin/cc
